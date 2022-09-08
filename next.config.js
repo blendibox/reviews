002b964d796aa7d17b3f248536d9323bef319711
Juliana Costa <jugcosta@ymail.com>
2022-09-07 23:04:25 -0300

    pagina inicial do app .next

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
   images: {
     domains: ['blendibox.com.br', 'cdn.awsli.com.br','cf.shopee.com.br','img.icons8.com'],
   },
+  env: {
+    PUBLIC_URL: "https://blendibox.github.io/reviews",
+    assetPrefix: './'
+  }
 }
 
 module.exports = nextConfig
